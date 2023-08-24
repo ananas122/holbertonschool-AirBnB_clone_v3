f58aeb51ef26b3bf6354e9e2f0824955011b3218
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-24 15:20:45 +0200

    Correction pycodestyle delete line at end of file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""""
+"""definitions of API """
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -30,4 +30,3 @@ def stats():
     }
 
     return jsonify(data)
-
