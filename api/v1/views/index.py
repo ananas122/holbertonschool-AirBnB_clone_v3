6d96ff18e492e160fad70023884e7f390e47e831
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-24 15:18:22 +0200

    Correction :delete quotation mark

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,12 +21,12 @@ def status():
 def stats():
     "return: JSON containing the count of each object type"
     data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
     }
 
     return jsonify(data)
