4421fd22c44ad5ed93a3d3d7cf7bf6dae00f5e1d
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-24 15:09:41 +0200

    ADD import Correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -24,3 +30,4 @@ def stats():
     }
 
     return jsonify(data)
+
