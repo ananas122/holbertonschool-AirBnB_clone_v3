0e4e8a872850048321e7553591a4ecd80965dee6
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-08-25 17:36:14 +0200

    Copy

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,41 +1,39 @@
 #!/usr/bin/python3
 """
-Module starting the API
+app.py to connect to API
 """
-from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
+import os
 from models import storage
-from os import getenv
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
-
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
-env_host = getenv('HBNB_API_HOST', '0.0.0.0')
-env_port = getenv('HBNB_API_PORT', 5000)
 
+# Register the blueprint containing the API routes
 app.register_blueprint(app_views)
-CORS(app, ressources={"/*": {"origins": "0.0.0.0"}})
 
 
+# Setup CORS to allow requests from any origin
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+
+# Teardown app context to close the database connection
 @app.teardown_appcontext
-def close(self):
-    """Clossing session method"""
+def teardown_appcontext(error):
+    """Teardown app context"""
     storage.close()
 
 
+# Handle 404 errors by returning a JSON response
 @app.errorhandler(404)
-def not_found(error):
-    """
-    Error handler for 404 errors that returns a JSON-formatted
-    404 status code response
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    """
-    Main flask app
-    """
-    app.run(host=env_host, port=env_port, threaded=True)
+    # Start the Flask app
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
+            
